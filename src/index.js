1bd5564178bb999589b4a8bf879e7093417024d7
Lambda_School_Loaner_85 <lambda_school_loaner_85@Lambda-School-Loaner-85s-MacBook-Pro.local>
2019-04-10 01:04:46 -0500

    Finished MVP, will review videos tomorrow and start on the second project

--- src/index.js
+++ src/index.js
@@ -3,21 +3,20 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import rootReducer from "./reducers";
-// needed dependancies
-// applyMiddleware from redux
-// thunk from redux-thunk
-// logger from redux-logger
-// rootReducer from ./reducers
+
 
 const store = createStore(
-  rootReducer
-  /* applyMiddleware goes here */
+  rootReducer, applyMiddleware(thunk, logger)
+
 );
 
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider 
+  store={store}>
     <App />
   </Provider>,
   document.getElementById("root")
--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { connect } from "react-redux";
-
 import { CharacterList } from "../components";
+import {fetchCharacters} from '../actions';
+
 // import actions
 
 class CharacterListView extends React.Component {
@@ -11,25 +12,36 @@ class CharacterListView extends React.Component {
 
   componentDidMount() {
     // call our action
+    this.props.dispatch(fetchCharacters())
   }
 
   render() {
+
     if (this.props.fetching) {
+      return <div>Loading...Loading...Loading...</div>
       // return something here to indicate that you are fetching data
     }
     return (
       <div className="CharactersList_wrapper">
-        <CharacterList characters={this.props.characters} />
+        <CharacterList 
+        characters={this.props.characters} />
+      
       </div>
     );
   }
 }
 
-// our mapStateToProps needs to have two properties inherited from state
-// the characters and the fetching boolean
-export default connect(
-  null /* mapStateToProps replaces null here */,
-  {
-    /* action creators go here */
+const mapStateToProps = state => {
+  console.log(state)
+  return {
+    characters: state.charsReducer.characters,
+    fetching: state.charsReducer.fetching,
+    error: state.charsReducer.error
   }
+}
+
+
+
+export default connect(
+  mapStateToProps
 )(CharacterListView);
